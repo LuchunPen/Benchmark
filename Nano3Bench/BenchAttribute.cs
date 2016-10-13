e24afd98dbf6f377608a57f2019ec0acf1805123
Luchunpen <anidorybp@gmail.com>
2016-10-13 19:00:09 +0300

    Nano3 bench - part of "Nano3"  lib

---
+++ Nano3Bench/BenchAttribute.cs
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) Luchunpen (bwolf88).  All rights reserved.
+Date: 29.07.2016 3:10:58
+*/
+
+using System;
+
+namespace Nano3.Diagnostics
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BenchAttribute : Attribute
+    {
+        private string _name;
+        private int _runCount;
+
+        public string Name { get { return _name; } }
+        public int RunCount { get { return _runCount; } }
+
+        public BenchAttribute(string name, int runCount)
+        {
+            if (runCount < 1) { throw new ArgumentOutOfRangeException("Run count < 1"); }
+
+            _name = name;
+            _runCount = runCount;
+        }
+    }
+}
---
+++ Nano3Bench/BenchResult.cs
@@ -0,0 +1,28 @@
+﻿/*
+Copyright (c) Luchunpen (bwolf88).  All rights reserved.
+Date: 29.07.2016 3:16:28
+*/
+
+using System;
+
+namespace Nano3.Diagnostics
+{
+    public class BenchResult
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+        private TimeSpan _elapsed;
+        public TimeSpan Elapsed { get { return _elapsed; } }
+
+        public BenchResult(string benchName, TimeSpan elapsed)
+        {
+            _name = benchName;
+            _elapsed = elapsed;
+        }
+
+        public override string ToString()
+        {
+            return Name + ": " + Elapsed.TotalMilliseconds;
+        }
+    }
+}
